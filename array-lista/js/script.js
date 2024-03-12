6e49767f16e2512efdc394de6ebc8511f2506621
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-12 16:21:07 +0100

    defined array fridge pushed "pesca" in to the fridge and created cycle for

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -10,5 +10,34 @@ Fasi
    - se c'è stampiamo: "Trovato! Devo solo preparare il cocktail."
    - se non lo trovo: "Oh no, devo uscire a comprare il cocomero!"
 Suggerimenti/Indicazioni:
-Sì, lo sappiamo che esiste la funzione includes() ma noi non la vogliamo utilizzare per cercare nel frigorifero.
+Sì, lo sappiamo che esiste la funzione includes() ma noi non la vogliamo utilizzare per cercare 
+nel frigorifero.
 */
+
+let fridge = [
+  "banana", //0
+  "mela", //1
+  "pera", //2
+  "ciliegia", //3
+  "arancia", //4
+  "mandarino", //5
+  "cocomero", //6
+  "limone", //7
+  "fragola", //8 conteggio array
+];
+
+fridge.push("pesca");
+let fruit = "cocomero"
+let flag = false
+// console.log(fridge);
+
+for (let i = 0; i < fridge.length; i++) {
+  if (fruit === fridge[i]) {
+    let flag = true
+  }
+}
+
+if (flag) {
+  console.log("Si ce l'ho");
+} else console.log("Oh no, devo uscire a comprare il cocomero!");
+// else console.log('Devi comprare il cocomero')
