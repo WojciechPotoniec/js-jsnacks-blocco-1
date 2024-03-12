cffa5d0ac7f596cf580df68da6d2ad3f0f4ac854
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-12 18:16:54 +0100

    print at first time the number of element that was input by user then take the last 5 number

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -2,6 +2,7 @@
 Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali 
 (compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
 Stampiamo poi gli ultimi 5 elementi dell'Array
+
 Bonus:
 Chiediamo all'utente quanti elementi dell'array dovremo stampare
 Suggerimenti:
@@ -18,5 +19,11 @@ usernumber = parseInt(prompt('Inserisci un numero'));
 
 for(let i = 0; i <= usernumber; i++){
     number.push(getRndInteger(1, 100));
-    console.log(number[i]);
 }
+console.log(number);
+console.log (number.slice(-5)); 
+
+/*if(number >= 5){
+    let numbers = number.slice(-5);     // number.slice(-5); ci servirà per prendere gli'ultimi 5 elementi
+        console.log(numbers);
+    }else console.log('Inserisci un valore maggiore di 5');*/
