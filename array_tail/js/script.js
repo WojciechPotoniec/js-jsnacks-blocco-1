f86a1673ff453a96aeb2431c48c5031bb3082b73
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-13 13:07:25 +0100

    fixed error

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,5 +1,7 @@
 /* Descrizione: 2. La coda dell'Array
-Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali 
+Creiamo un array 
+chiedendo all'utente quanti elementi dovrà contenere
+e generando tanti numeri casuali 
 (compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
 Stampiamo poi gli ultimi 5 elementi dell'Array
 
@@ -10,20 +12,19 @@ Probabilmente conviene controllare che il numero di elementi della coda non sia 
 del numero di elementi totali dell'array.
 */
 
-
 //devo creare una lista di elementi chiedendo all'user il numero degli elementi.
 
+let number = [];
+let usernumber = parseInt(prompt("Inserisci un numero di elementi da inserire in lista"));
 
-number = [];
-usernumber = parseInt(prompt('Inserisci un numero'));
-
-for(let i = 0; i <= usernumber; i++){
-    number.push(getRndInteger(1, 100));
+for (let i = 0; i < usernumber; i++) {
+  number.push(getRndInteger(1, 100));
 }
-console.log(number);
-console.log (number.slice(-5)); 
-// number.slice(-5); ci servirà per prendere gli'ultimi 5 elementi partendo dall'ultimo
 
-/*if(number < usernumber){
-    console.log(prompt(Quanti numeri vuoi visualizzare?));
-}*/
+let useroutput = parseInt(
+  prompt("Quanti numeri della lista vuoi visualizzare?")
+);
+if (number.length < useroutput) {
+  console.log("Errore, il numero che vuoi visualizzare non può essere maggiore della lista degli elementi inseriti inizialmente");
+} else console.log(number.slice(-useroutput));
+// number.slice(); ci servirà per prendere un tot numero di elementi
