34c94dbd97ffed7325ab296ccfa7c448fdb1d402
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-13 14:10:06 +0100

    created 2 array with different length, then used for cycle to increment the number2length

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -3,3 +3,17 @@ Cartella: array_bonus
 Crea due array che hanno un numero di elementi diversi.
 Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 */
+
+let number = [1, 2, 3, 4, 5];
+let number2 = [10, 20, 30];
+// let newnumber = [number.length - number2.lenght]; // determino la lunghezza della differenza tra i due array
+
+console.log(number.length);
+console.log(number2.length);
+// console.log(newnumber);
+
+for (number2.lenght = 3; number2.length < number.length; number2.length++) {
+  let newnumber = (number2.length + 1);
+  console.log(newnumber);
+}
+
